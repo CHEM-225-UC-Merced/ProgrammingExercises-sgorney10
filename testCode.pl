bc75b0f696b98797a600a8fd6ef091fe5e84b028
Hrant P. Hratchian <hhratchian@ucmerced.edu>
2025-02-22 17:37:21 -0800

    Updated gradingNotes.txt for Programming Exercise 1 grading. Also, made updates to testCode.pl.

--- testCode.pl
+++ testCode.pl
@@ -1,40 +1,52 @@
 #!/usr/bin/perl
 #
 #   This perl script is used to test Programming Exercises in CHEM 225.
+#   Currently, the script is set up to check compilation and unit tests for
+#   Programming Excercise 1.
 #
 #   
-#   H. P. Hratchian
+#   H. P. Hratchian, 2/6/2025
 #   hhratchian@ucmerced.edu
 #
 #
-#   Programming Exercise 1.
+#   Default the problem number.
 #
-#   1-1.
-    system("echo input_01_01.txt | ./prgm_01_01.exe >& .outfiles/out_01_01.out");
-    $temp = `diff .outfiles/out_01_01.out .outfiles/out_01_01.txt`;
-    if($temp){
-      push(@scores,"**FAIL**");
+    $problemNumber = 1;
+#
+#   Build the test table results array for the requested problem.
+#
+    if($problemNumber == 1){
+#     Programming Exercise 1.
+#
+#     1-1.
+      system("printf 'input_01_01.txt\n' | ./prgm_01_01.exe > .outfiles/out_01_01.out");
+      $temp = `diff .outfiles/out_01_01.out .outfiles/out_01_01.txt`;
+      if($temp){
+        push(@scores,"**FAIL**");
+      }else{
+        push(@scores,"PASS");
+      }
+#
+#     1-2.
+      system("printf 'input_01_01.txt\ninput_01_02.txt\n' | ./prgm_01_02.exe > .outfiles/out_01_02.out");
+      $temp = `diff .outfiles/out_01_02.out .outfiles/out_01_02.txt`;
+      if($temp){
+        push(@scores,"**FAIL**");
+      }else{
+        push(@scores,"PASS");
+      }
+#
+#     1-3.
+      system("printf 'input_01_01.txt\ninput_01_02.txt\n' | ./prgm_01_03.exe > .outfiles/out_01_03.out");
+      $temp = `diff .outfiles/out_01_03.out .outfiles/out_01_03.txt`;
+      if($temp){
+        push(@scores,"**FAIL**");
+      }else{
+        push(@scores,"PASS");
+      }
     }else{
-      push(@scores,"PASS");
+      die "\n\nUnknown problem number requested.\n\n";
     }
-#   1-2.
-    system("echo -e \"input_01_01.txt\ninput_01_02.txt\" | ./prgm_01_02.exe > .outfiles/out_01_02.out");
-    $temp = `diff .outfiles/out_01_02.out .outfiles/out_01_02.txt`;
-    if($temp){
-      push(@scores,"**FAIL**");
-    }else{
-      push(@scores,"PASS");
-    }
-#   1-2.
-    system("echo -e \"input_01_01.txt\ninput_01_02.txt\" | ./prgm_01_03.exe > .outfiles/out_01_03.out");
-    $temp = `diff .outfiles/out_01_03.out .outfiles/out_01_03.txt`;
-    if($temp){
-      push(@scores,"**FAIL**");
-    }else{
-      push(@scores,"PASS");
-    }
-
-
 #
 #   Print out result table.
 #
